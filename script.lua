7b2e1b1bfbbd4084de041dbe91a46f12bfe5d565
heap <68669985+heap-v@users.noreply.github.com>
2022-03-25 15:39:55 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -2510,39 +2510,6 @@ pcall(function()
           Duration = 20;
          }
        setclipboard('https://dsc.gg/w4nted')
-
- local webhookcheck =
-   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
-   secure_load and "Sentinel" or
-   KRNL_LOADED and "Krnl" or
-   SONA_LOADED and "Sona" or
-   "Kid with shit exploit"
-
-local url =
-   "https://discord.com/api/webhooks/956939540537417759/l0KDKKYNw83dRm3DVKz7G60v4V8m10ytNqMmwPxLC_8s-Q_tChC5AuO4stDQxHYEjfQX"
-local data = {
-   ["content"] = " message(no embed)- you can take out embed if by deleting the bottom stuff(where it says EMBEDS)",
-   ["embeds"] = {
-       {
-           ["title"] = "**Someone Executed Your Script!**",
-           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
-           ["type"] = "rich",
-           ["color"] = tonumber(0x7269da),
-           ["image"] = {
-               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
-                   tostring(game:GetService("Players").LocalPlayer.Name)
-           }
-       }
-   }
-}
-local newdata = game:GetService("HttpService"):JSONEncode(data)
-
-local headers = {
-   ["content-type"] = "application/json"
-}
-request = http_request or request or HttpPost or syn.request
-local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-request(abcdef)
       end))
     end
   end)
